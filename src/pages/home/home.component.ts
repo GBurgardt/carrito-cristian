92e04e70c8cd1c74c63019a59ed99b04008aea8f
German Burgardt <germanburgardt@pop-os.localdomain>
2019-05-22 17:37:03 -0300

    First commit

---
+++ src/pages/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+    title = 'carrito';
+
+    test: boolean;
+
+    fafa() {
+
+        const arr = [1,2,4,3,5];
+
+
+
+    }
+
+
+    sortArray(arr) {
+        // ajsdjosadoj lod
+        return arrayOrdenado;
+    }
+
+
+}
